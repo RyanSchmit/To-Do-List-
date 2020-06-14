891cae933e36b32a3d169072573e48dc5c1d9d08
RyanSchmit <48778033+RyanSchmit@users.noreply.github.com>
2020-06-14 01:11:20 -0400

    Added new item function

--- src/App.js
+++ src/App.js
@@ -2,16 +2,63 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state={
+      newItem:"",
+      list:[]
+    }
+  }
+
+  updateInput(key, value){
+    this.setState({
+      [key]: value
+    })
+  }
+  addItem() {
+    //create id for new items
+    const newItem={
+      id: 1 + Math.random(),
+      value: this.state.newItem.slice()
+    };
+    //copying current list
+    const list = [...this.state.list];
+
+    //add new item to list 
+    list.push(newItem);
+
+    //update list 
+    this.setState({
+      list,
+      newItem:""
+    })
+  }
   
   render() {
       return (
       <div className="main">
         <h1 className="head">Get It Done</h1>
         <div className="input">
-          <input className="thingsToDo" type="text" placeholder="Enter things you have to do"></input>
-          <button>Create List</button>
-        </div>
         <button>Hide/Show</button>
+        <br></br>
+        <input
+            type="text"
+            placeholder="Enter things you have to do"
+            className="thingsToDo"
+            value={this.state.newItem}
+            onChange={e => this.updateInput("newItem", e.target.value)}
+          />
+          <button onClick={() => this.addItem()}>Create List</button>
+          <br></br>
+            {this.state.list.map(item => {
+              return (
+                <p key={item.id}>
+                  {item.value}
+                </p>
+              );
+            })}
+        </div>
         <p id="demo"></p>
       </div>
     );
